b8b41a456297d0cf79ff58c85eecb78850b6fc52
Bruno Dechen <45558134+d3ch3n@users.noreply.github.com>
2026-03-22 16:40:16 -0300

    uploaded

--- info.lua
+++ info.lua
@@ -1,8 +1,8 @@
 PluginInfo = {
   Name = "DCH~CameraVISCA",
   Description = "VISCA Camera with IP/RS232 Control",
-  Version = "2.3.0.9",
-  BuildVersion = "2.3.0.9",
+  Version = "2.3.0.10",
+  BuildVersion = "2.3.0.10",
   Id = "7c307beb-8afc-4029-9f5d-674baa4bd89d",
   Author = "Extrasolve, Isaac Porter, Bruno Dechen",
 } 
